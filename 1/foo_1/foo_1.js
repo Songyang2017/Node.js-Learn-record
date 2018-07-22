64e0d81b0b609fc71f0dcc4e64c3cd6d1d7529c5
Songyang2017 <1546302993@qq.com>
2018-07-22 17:49:24 +0800

    lesson3

--- 1/foo_1/foo_1.js
+++
@@ -1,8 +0,0 @@
-var str_1 = '这是foo_1模块'
-
-exports.print = function(str){
-	console.log(str);
-	console.log(str_1)
-	console.log(this);
-	console.log('--------------');
-}
--- 1/index.js
+++
@@ -1,6 +0,0 @@
-var foo_1 = require('./foo_1/foo_1')
-var foo_2 = require('./foo_2/foo_2')
-	
-var st = '复仇者联盟'	
-foo_1.print(st)
-console.log(this);
